9a4d73ec7d2f258e961636b5bdcef845ad053f4e
Kerr Marin Miller <kerr@kerrmarin.com>
2016-02-15 11:45:48 +0000

    Update podspec

--- Pod/Classes/Parser/GhostLoginTokenJSONParser.swift
+++ Pod/Classes/Parser/GhostLoginTokenJSONParser.swift
@@ -14,15 +14,18 @@ public class GhostLoginTokenJSONParser: GhostLoginTokenParser {
 
     public func tokenFromResponse(response: AnyObject) throws -> GhostLoginToken? {
         guard let responseDictionary = response as? [String : AnyObject] else {
-            throw ParseErrorType.ParsingError(code: .InvalidJSON, detail: "Error Parsing JSON - JSON was not valid.")
+            throw ParseErrorType.ParsingError(code: .InvalidJSON,
+                detail: "Error Parsing JSON - JSON was not valid.")
         }
         
         guard let accessToken = responseDictionary["access_token"] as? String else {
-            throw ParseErrorType.ParsingError(code: .UnexpectedTypes, detail: "Error Parsing JSON - access_token was not a string")
+            throw ParseErrorType.ParsingError(code: .UnexpectedTypes,
+                detail: "Error Parsing JSON - access_token was not a string")
         }
         
         guard let expiryNumber = responseDictionary["expires_in"] as? NSNumber where expiryNumber.integerValue > 0 else {
-            throw ParseErrorType.ParsingError(code: .UnexpectedTypes, detail: "Error Parsing JSON - expires_in was either not a number or less than or equal to zero")
+            throw ParseErrorType.ParsingError(code: .UnexpectedTypes,
+                detail: "Error Parsing JSON - expires_in was either not a number or less than or equal to zero")
         }
         
         let token = GhostLoginToken(accessToken: accessToken, expiry: expiryNumber.integerValue)
